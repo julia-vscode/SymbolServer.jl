4ee70e33733f9c3b65230362dd9861ab1a8f685c
ZacNugent <zac.nugent@gmail.com>
2019-04-03 22:39:18 +0100

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,9 @@ using Test
 
 @testset "SymbolServer" begin
     s = SymbolServerProcess()
-
     @test isa(s, SymbolServerProcess)
+    SymbolServer.getstore(s)
+    @test haskey(s.depot, "Base")
+    @test haskey(s.depot, "Core")
+    @test haskey(s.depot, "SymbolServer")
 end
