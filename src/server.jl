78a536be91e8f612efdd2e33f07d9a33c0ddd5e0
David Anthoff <anthoff@berkeley.edu>
2020-01-13 12:38:09 -0800

    Lower symbol server priority

--- src/server.jl
+++ src/server.jl
@@ -1,5 +1,23 @@
 module SymbolServer
 
+# Try to lower the priority of this process so that it doesn't block the
+# user system.
+@static if Sys.iswindows()
+    # Get process handle
+    p_handle = ccall(:GetCurrentProcess, stdcall, Ptr{Cvoid}, ())
+
+    # Set BELOW_NORMAL_PRIORITY_CLASS, this only affects compute stuff
+    ret = ccall(:SetPriorityClass, stdcall, Cint, (Ptr{Cvoid}, Culong), p_handle, 0x00004000)
+    ret!=1 && @warn "Something went wrong when setting BELOW_NORMAL_PRIORITY_CLASS."
+
+    # Also set PROCESS_MODE_BACKGROUND_BEGIN, this affects IO (and maybe also CPU?)
+    ret = ccall(:SetPriorityClass, stdcall, Cint, (Ptr{Cvoid}, Culong), p_handle, 0x00100000)
+    ret!=1 && @warn "Something went wrong when setting PROCESS_MODE_BACKGROUND_BEGIN."
+else
+    ret = ccall(:nice, Cint, (Cint, ), 1)
+    # We don't check the return value because it doesn't really matter
+end
+
 module LoadingBay
 end
 
