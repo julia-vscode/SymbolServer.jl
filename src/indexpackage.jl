9fe54d72c284c8c4c51242be1180573815cd99c4
Zac Nugent <zac.nugent@gmail.com>
2021-01-13 22:16:03 +0000

    revert change to include style, strip paths

--- src/indexpackage.jl
+++ src/indexpackage.jl
@@ -10,7 +10,11 @@ Pkg.add(name=current_package_name, version=current_package_version)
 
 
 # TODO Make the code below ONLY write a cache file for the package we just added here.
-include("./SymbolServer.jl")
+include("faketypes.jl")
+include("symbols.jl")
+include("utils.jl")
+include("serialize.jl")
+using .CacheStore
 
 # This path will always be mounted in the docker container in which we are running
 store_path = "/tmp/symcache"
@@ -24,15 +28,17 @@ catch e
 end
 
 # Get the symbols
-env = SymbolServer.getenvtree([Symbol(current_package_name)])
-SymbolServer.symbols(env, m)
+env = getenvtree([Symbol(current_package_name)])
+symbols(env, m)
 
 # Write them to a file
 ctx = Pkg.Types.Context()
-uuid = SymbolServer.packageuuid(ctx, current_package_name)
+uuid = packageuuid(ctx, current_package_name)
+modify_dirs(env[Symbol(current_package_name)], f -> modify_dir(f, pkg_src_dir(Base.loaded_modules[Base.PkgId(uuid, current_package_name)]), "PLACEHOLDER")) # Strip out paths
+# There's an issue here - @enum used within CSTParser seems to add a method that is introduced from Enums.jl...
 mkpath(joinpath(store_path, "v1", "packages", "$(current_package_name)_$uuid"))
 versionwithoutplus = replace(string(current_package_version), '+'=>'_')
 cache_path = joinpath(store_path, "v1", "packages", "$(current_package_name)_$uuid", "v_$(versionwithoutplus)_$current_package_treehash.jstore")
 open(cache_path, "w") do io
-    SymbolServer.CacheStore.write(io, SymbolServer.Package(current_package_name, env[Symbol(current_package_name)], uuid, nothing))
+    CacheStore.write(io, Package(current_package_name, env[Symbol(current_package_name)], uuid, nothing))
 end
