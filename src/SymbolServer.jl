3a5556e53793afa7ed111c1e2977b633d95da9b2
Zac Nugent <zac.nugent@gmail.com>
2021-01-19 22:47:32 +0000

    add kw arg to turn off downloading of caches

--- src/SymbolServer.jl
+++ src/SymbolServer.jl
@@ -23,16 +23,18 @@ mutable struct SymbolServerInstance
     end
 end
 
-function getstore(ssi::SymbolServerInstance, environment_path::AbstractString, progress_callback=nothing, error_handler=nothing)
+function getstore(ssi::SymbolServerInstance, environment_path::AbstractString, progress_callback=nothing, error_handler=nothing; download = true)
     !ispath(environment_path) && return :success, recursive_copy(stdlibs)
 
     # see if we can download any package cache's before 
-    manifest_filename = isfile(joinpath(environment_path, "JuliaManifest.toml")) ? joinpath(environment_path, "JuliaManifest.toml") : joinpath(environment_path, "Manifest.toml")
-    if isfile(manifest_filename)
-        let manifest = Pkg.Types.read_manifest(manifest_filename)
-            asyncmap(collect(validate_disc_store(ssi.store_path, manifest)), ntasks = 10) do pkg
-                uuid = packageuuid(pkg)
-                suc = get_file_from_cloud(manifest, uuid, environment_path, ssi.depot_path, ssi.store_path, ssi.store_path)
+    if download
+        manifest_filename = isfile(joinpath(environment_path, "JuliaManifest.toml")) ? joinpath(environment_path, "JuliaManifest.toml") : joinpath(environment_path, "Manifest.toml")
+        if isfile(manifest_filename)
+            let manifest = Pkg.Types.read_manifest(manifest_filename)
+                asyncmap(collect(validate_disc_store(ssi.store_path, manifest)), ntasks = 10) do pkg
+                    uuid = packageuuid(pkg)
+                    suc = get_file_from_cloud(manifest, uuid, environment_path, ssi.depot_path, ssi.store_path, ssi.store_path)
+                end
             end
         end
     end
