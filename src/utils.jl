76116e8bd0037790b83af0190765f4a0047426d9
Zac Nugent <zac.nugent@gmail.com>
2021-02-17 11:47:51 +0000

    fix

--- src/utils.jl
+++ src/utils.jl
@@ -434,15 +434,22 @@ function get_file_from_cloud(manifest, uuid, environment_path, depot_dir, cache_
     name = packagename(manifest, uuid)
     link = string(first(splitext(joinpath("https://www.julia-vscode.org/symbolcache/store/v1/packages", paths...))), ".tar.gz")
     dest_filepath = joinpath(cache_dir, paths...)
+    dest_filepath_unavailable = string(first(splitext(dest_filepath)), ".unavailable")
     download_dir = joinpath(download_dir, first(splitext(last(paths))))
     download_filepath = joinpath(download_dir, last(paths))
+    download_filepath_unavailable = string(first(splitext(download_filepath)), ".unavailable")
+    if isfile(download_filepath_unavailable)
+        @info "Cloud was unable to cache $name in the past, we won't try to retrieve it again."
+        return false
+    end
     file = try
         if Pkg.PlatformEngines.download_verify_unpack(link, nothing, download_dir)
             !isdir(joinpath(cache_dir, paths[1])) && mkdir(joinpath(cache_dir, paths[1]))
             !isdir(joinpath(cache_dir, paths[1], paths[2])) && mkdir(joinpath(cache_dir, paths[1], paths[2]))
-            if !isfile(download_filepath) && isfile(string(first(splitext(download_filepath)), ".unavailable"))
-                CacheStore.write(open(dest_filepath, "w"), ModuleStore(VarRef(nothing, Symbol(name)), Dict{Symbol,Any}(), "$(name)@$(version(manifest[uuid])) was downloaded but was not successfully cached.", true, Symbol[], Symbol[]))
-                return true
+            if !isfile(download_filepath) && isfile(download_filepath_unavailable)
+                mv(download_filepath_unavailable, dest_filepath_unavailable)
+                rm(download_dir)
+                return false
             end
             mv(download_filepath, dest_filepath)
             rm(download_dir)
